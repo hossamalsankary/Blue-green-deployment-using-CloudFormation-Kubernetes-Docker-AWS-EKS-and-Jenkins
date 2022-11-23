d67528c2d84965ef761233ab55e7b23f72fc9429
hossamalsankary <35647042+hossamalsankary@users.noreply.github.com>
2022-11-23 10:06:18 +0200

    update

--- bash-scripts/smokeTest.sh
+++ bash-scripts/smokeTest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export blueIP=$(kubectl get services --namespace=blue-deployment  blueservice --output jsonpath='{.status.loadBalancer.ingress[0]}'| tr -d '"' | tr -d '}' | cut -d ':' -f 2 )
 export greenIP=$(kubectl get services --namespace=green-deployment  greenservice --output jsonpath='{.status.loadBalancer.ingress[0]}'| tr -d '"' | tr -d '}' | cut -d ':' -f 2 )
-curl '$blueIP:8080'
-curl '$greenIP:8080'
+curl 'http://$blueIP:8080'
+curl 'http://$greenIP:8080'
